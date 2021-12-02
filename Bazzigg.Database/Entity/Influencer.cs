8df024b07051a33fb74dcc07d4e1a30084bac472
Plorence <ploseok@gmail.com>
2021-12-02 17:02:03 +0900

    테이블 추가

---
+++ Bazzigg.Database/Entity/Influencer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bazzigg.Database.Entity
+{
+    public class Influencer
+    {
+        public string AccessId { get; set; }
+        public string Nickname { get; set; }
+        public string Description { get; set; }
+        public List<string> Keywords { get; set; }
+        public string YoutubeLink { get; set; }
+        public string TwitchLink { get; set; }
+    }
+}
