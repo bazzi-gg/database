287dd395bd083b39fde7644cfeefad16bc4c665f
Plorence <ploseok@gmail.com>
2021-12-02 17:02:37 +0900

    Influencer 테스트 코드 생성

---
+++ Bazzigg.Database.Test/InfluencerTests.cs
@@ -0,0 +1,34 @@
+﻿using Bazzigg.Database.Context;
+using Bazzigg.Database.Entity;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bazzigg.Database.Test
+{
+    [TestClass]
+    public class InfluencerTests
+    {
+        [TestMethod("추가")]
+        public void Add()
+        {
+            using var context = new AppDbContext(TestOptions.AppDbContextOptions);
+            var obj = new Influencer()
+            {
+                AccessId = "",
+                Description = "",
+                Keywords = new List<string>()
+                {
+                    "", ""
+                },
+                Nickname = ""
+            };
+            context.Influencer.Add(obj);
+            context.SaveChanges();
+        }
+
+    }
+}
